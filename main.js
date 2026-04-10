3536a6b46c4efdffa0ae0a43a73d7dadef13f6c5
MykhailoBul <mykhailo.bulava@voco.ee>
2026-04-10 18:15:47 +0300

    kontakt

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ $(function (){
         $('.navigation').toggleClass('nav_open');
     });
 });
+
+function copyT(element){
+    const text = element.innerText;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Text copied')
+    });
+};
